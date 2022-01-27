30f6257c24f0b6f8bf6cf0c0ac88eefc7a02f1cd
Jakub Zdanowicz <jakub.zdanowicz99@gmail.com>
2022-01-27 17:04:16 +0100

    2nd

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <hello-world/>
     <movie-list/>
   </div>
 </template>
 
 <script>
 import MovieList from "@/components/MovieList";
+import "bootstrap/dist/css/bootstrap.css"
 
 export default {
   name: 'App',
@@ -16,13 +17,3 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,42 +1,117 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>Title</th>
-        <th>Year</th>
-        <th>Nominations</th>
-        <th>Wins</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="movie in movies" :key="movie.id">
-        <td>{{movie.title}}</td>
-        <td>{{movie.year}}</td>
-        <td>{{movie.nominations}}</td>
-        <td>{{movie.wins}}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="overflow-auto">
+
+    <div>
+      <b-form-input v-model="title" type="text" placeholder="Enter the movie title"></b-form-input>
+      <b-form-input v-model="year" type="number" placeholder="Enter the year it was made"></b-form-input>
+      <b-form-input v-model="nominations" type="number" placeholder="Enter how many nominations it got"></b-form-input>
+      <b-form-input v-model="wins" type="number" placeholder="Enter how many wins it got"></b-form-input>
+      <b-button v-on:click="addMovie" pill variant="success">Add a movie</b-button>
+      <b-form-input v-model="id" type="number" placeholder="Movie Id"></b-form-input>
+      <b-button v-on:click="deleteMovie" pill variant="danger">Delete</b-button>
+    </div>
+
+  <div class="b-table-responsive-sm">
+    <b-table bordered striped hover
+        id="my-table"
+        :items="movies"
+        :fields="fields"
+        :per-page="perPage"
+        :current-page="currentPage"
+        default>
+    </b-table>
+  </div>
+
+
+    <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="my-table"
+    ></b-pagination>
+
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
+const baseURL = "http://localhost:3000/movies/";
+
 export default {
   name: "movie-list",
   data() {
     return {
-      movies: []
+      fields: [
+        {
+          key: 'id'
+        },
+        {
+          key: 'title',
+          sortable: true
+        },
+        {
+          key: 'year',
+          sortable: true
+        },
+        {
+          key: 'nominations',
+          sortable: true,
+          // variant: 'danger'
+        },
+        {
+          key: "wins",
+          sortable: true,
+        }
+      ],
+      perPage: 15,
+      currentPage: 1,
+      movies: [],
+      title: "",
+      year: "",
+      nominations: "",
+      wins: "",
+      id: "",
     }
   },
-  created() {
-    fetch("http://localhost:3000/movies")
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        this.movies = data;
-      })
+  async created() {
+    try {
+      const res = await axios.get(baseURL);
+
+      this.movies = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  computed: {
+    rows() {
+      return this.movies.length
+    }
+  },
+  methods: {
+    async addMovie() {
+      const res = await axios.post(baseURL, {
+        title: this.title,
+        year: this.year,
+        nominations: this.nominations,
+        wins: this.wins
+      });
+
+      this.movies = [...this.movies, res.data];
+
+      this.movies = "";
+      this.title = "";
+      this.year = "";
+      this.nominations = "";
+      this.wins = "";
+    },
+    async deleteMovie() {
+      await axios.delete(baseURL + this.id);
+      this.id = "";
+      this.created();
+    }
   }
-}
+};
 </script>
 
 <style scoped>
